f4a66fd1b73c33f42e5f41fce9df45c69e51f822
nurikUrgench <113215297+nurikUrgench@users.noreply.github.com>
2022-12-17 13:50:38 +0300

    Add files via upload

---
+++ lesson_22/js/script.js
@@ -0,0 +1,23 @@
+'use strict'
+
+let tUndef;
+console.log(typeof tUndef);
+console.log(tUndef);
+
+let tNull = null;
+console.log(typeof tNull);
+
+console.log(tNull);
+
+let user = 'Вася';
+
+let userName = `Nuriddin`;
+let userAge = 22;
+let userMail = `sapaev@gmail.com`;
+let userInfo = `Имя: ${userName}, Возраст: ${userAge}, почта: ${userMail} `;
+console.log(typeof userInfo);
+console.log(userInfo);
+console.log(user);
+
+let myUser = "frilance ${userAge}";
+console.log(myUser);
